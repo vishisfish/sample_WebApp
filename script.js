98905f24b45597150a8251af4eff6856403e10d7
Vishvabala <94888653+vishisfish@users.noreply.github.com>
2022-07-11 14:51:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function setCookie(){
-document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+document.cookie = "username=John Doe; expires=Thu, 18 Dec 2022 12:00:00 UTC";
 console.log("HELLO");
 }
